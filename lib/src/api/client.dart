08814ce3d1867fc5352ec52f8eadd64214375f51
aliammar <aliammarali125@gmail.com>
2023-03-22 16:18:28 +0300

    remove unneeded package

--- lib/src/api/client.dart
+++ lib/src/api/client.dart
@@ -1,20 +1,12 @@
 import 'package:dio/dio.dart';
-import 'package:dio_smart_retry/dio_smart_retry.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:starter/src/api/auth.dart';
 import 'package:starter/src/api/user.dart';
 import 'package:starter/src/config/constant.dart';
 
-import '../shared/helper/log.dart';
-
 final dioClient = Provider((ref) {
   final dio = Dio(BaseOptions(
     baseUrl: baseUrl,
-  ));
-  // config dio here
-  dio.interceptors.add(RetryInterceptor(
-    dio: dio,
-    logPrint: logIt,
   ));
 
   return dio;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,9 @@ import Foundation
 import flutter_secure_storage_macos
 import path_provider_macos
 import sqflite
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
